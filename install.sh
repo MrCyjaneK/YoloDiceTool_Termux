737ca2b232c099a4dc5e715b8c27fe412d1510dc
Cyjan <MrCyjaneK@Gmail.com>
2019-08-29 15:28:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
-apt update
-apt upgrade -y
-apt install curl nodejs php git -y
-git clone https://github.com/earnwallet/YoloDiceTool_Termux yd
-cd yd
-chmod +x YoloScript.sh
-cd ..
-echo "Complete! to Start script use:\n\n\n\n\n\n\n"
-echo "cd ~/yd; sh YoloScript.sh"
+apt update.
+# No special requirements...
+# node_modules included.
+#apt upgrade -y
+#apt install curl nodejs php git -y
+#git clone https://github.com/earnwallet/YoloDiceTool_Termux yd
+#cd yd
+#chmod +x YoloScript.sh
+#cd ..
+#echo "Complete! to Start script use:\n\n\n\n\n\n\n"
+#echo "cd ~/yd; sh YoloScript.sh"
